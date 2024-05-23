fe2a8ddfe26937a76c03a8409407be9e80c401eb
iabad1 <63217881+iabad1@users.noreply.github.com>
2024-05-22 20:43:05 -0700

    Unfinished Featured Logic

--- blog-react-app/src/components/FeaturedBlogs/FeaturedBlogs.jsx
+++ blog-react-app/src/components/FeaturedBlogs/FeaturedBlogs.jsx
@@ -6,30 +6,50 @@ const FeaturedBlogs = ({featuredBlogs}) =>{
     
 
     const FEATURED_TOTAL = 5;
-    let FEATURED_BLOG_COUNT = 0;
+    const FEATURED_BLOG_COUNT = featuredBlogs.length;
     //const [count, setCount] = useState(0);
     
-    
+    /*
     featuredBlogs.forEach(oneBlog=>{
        if( oneBlog.isFeatured){ FEATURED_BLOG_COUNT++ }
     })
-
-    const remain_featured_count = FEATURED_TOTAL - FEATURED_BLOG_COUNT;
+    */
+    
+    const REMAIN_COUNT = FEATURED_TOTAL - FEATURED_BLOG_COUNT;
+    //setting initial values 
+    const [remainCount, setRemainCount] = useState(REMAIN_COUNT);
+    const [currentSelect, setCurrentSelect] = useState(FEATURED_BLOG_COUNT);
+    
+    const setChecked = (checked) =>{
+        //check if the current input is selected or not
+        if(remainCount >= 0 && remainCount <= 5){
+            if(checked){
+                setCurrentSelect(currentSelect + 1);
+            }
+            else{
+                setCurrentSelect(currentSelect - 1);
+            }
+            setRemainCount(FEATURED_TOTAL - currentSelect);
+            
+        }
+        
+    }
   
 
     return(
         
         <div className="blogs-list-container">
-        <p>{remain_featured_count} remaining featured blogs</p>
+        <p>{FEATURED_BLOG_COUNT}/5 remaining featured blogs</p>
         
             {
                 featuredBlogs.map((oneBlog) =>(
+                
                 
                 <div className="blogs-list">
             
                     <label>
                         <input type='checkbox' name='blogCheckBox' defaultChecked={oneBlog.isFeatured} 
-                            disabled={remain_featured_count === 0}/>
+                            disabled={remainCount === 0} onChange={e=> setChecked(e.target.checked)}/>
                         {oneBlog.title}
                     </label>
                 </div>
